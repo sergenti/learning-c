da40b245835f2193fdc8e50ff55407cd2be4986a
Fylls <sergentifilippo@gmail.com>
2020-10-01 01:32:43 +0200

    a b c

---
+++ beginner/descriminant.c
@@ -0,0 +1,11 @@
+#include <math.h>
+
+int main()
+{
+    double a, b, c;            // inputs
+    double disc, root1, root2; // outputs
+
+    disc = pow(b, 2) - 4 * a * c;
+    root1 = (-b + sqrt(disc)) / (2 * a);
+    root2 = (-b - sqrt(disc)) / (2 * a);
+}
