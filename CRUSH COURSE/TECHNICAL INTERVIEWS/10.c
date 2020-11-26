3722fd1a35f7de5de91cfa7cd72954d0384e8f8e
Fylls <sergentifilippo@gmail.com>
2020-11-26 14:11:14 +0100

    Update 10.c

--- CRUSH COURSE/TECHNICAL INTERVIEWS/10.c
+++ CRUSH COURSE/TECHNICAL INTERVIEWS/10.c
@@ -9,7 +9,9 @@ void main()
 
 // ++var PRE-increment operator, unary operator
 
-// ERROR MESSAGE: compile-time error
+// ERROR MESSAGE: compile-time error [Lvalue required] (left side value)
+// ++2 results to 3 but computer isn't able to store 3 in 2 since it is not a variable
+
 // ++(a+b) s also an error
 
 // can only be used with variables
