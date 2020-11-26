81f0d10fe0b6a90c3ea6957ceda6de073fb19d34
Fylls <sergentifilippo@gmail.com>
2020-11-26 23:49:21 +0100

    Update vectorNormalization.c

--- ALGORITHMS/math/vectorNormalization.c
+++ ALGORITHMS/math/vectorNormalization.c
@@ -22,12 +22,11 @@ void printVector(int DIMENSIONS, double vector[])
     printf("\n");
     for (int i = 0; i < DIMENSIONS; i++)
         printf("vector[%d] = %f\n", i, vector[i]);
-    printf("\n");
 }
 
 void scanVector(int *DIMENSIONS, double vector[])
 {
-    printf("in how many dimensions do you want to work in ? ");
+    printf("in how many dimensions do you want to work in ?");
     scanf("%d", DIMENSIONS);
 
     // Add components to vector
@@ -42,17 +41,15 @@ void scanVector(int *DIMENSIONS, double vector[])
 
 void normalizeVector(int DIMENSIONS, double vector[], double normalizedVector[])
 {
-    // Calculate normalizationFactor
     double normalizationFactor;
-    double sum = 0.0;
+
+    double sum = 0;
 
     for (int i = 0; i < DIMENSIONS; i++)
-        printf("%f", vector[0]);
-    // sum += vector[i] * vector[i];
+        sum += pow(vector[i], 2);
 
-    // printf("%f",sum);
     normalizationFactor = sqrt(sum);
-    //printf("%f",normalizationFactor);
+
     for (int i = 0; i < DIMENSIONS; i++)
         normalizedVector[i] = vector[i] / normalizationFactor;
 
